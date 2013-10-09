39447789d2333e7a605ef10148c62bfee18ec59a
Ahmed ElSayed <ahmels@microsoft.com>
2013-10-09 15:21:20 -0700

    SPA5 preview app

---
+++ Preview_Spa5/Models/TodoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Preview_Spa5.Models
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public TodoDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<TodoList> TodoLists { get; set; }
+    }
+}
